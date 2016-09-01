60b1d4207b59fe2cfb443c5c20f2f2e28b63f8ff
J_SEAES_L <J_SEAES_L@live.cn>
2016-09-01 09:24:09 +0800

    [build]

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace len1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "ki";
+            int hp = 100;
+            int level = 11;
+            float exp = 12.3f;
+            long l = 9999l;
+            Console.WriteLine(" muName: \"{0}\" \n hp:{1} \n level:{2} \n exp: {3}", name, hp, level, exp);
+            Console.ReadKey();
+        }
+    }
+}
